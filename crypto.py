c71cd16e742d98014316efdaad8ff79d23ee6afb
Mankurt0 <183689932+mankurt0@users.noreply.github.com>
2025-02-15 21:04:52 +0300

    Функция хешировки пароля

--- crypto.py
+++ crypto.py
@@ -1,16 +1,26 @@
 from cryptography.fernet import Fernet
 import bcrypt
+from interface import entlogin, entpassword
 
-key = Fernet.generate_key() 
+def hashpass(password: str) -> bytes:
+    encpassword = password.encode()
+    hashedpassword = bcrypt.hashpw(encpassword, bcrypt.gensalt())
+    return hashedpassword
+
+print(hashpass(entpassword))
+
+""" key = Fernet.generate_key() 
 f = Fernet(key)
 message = "Сообщение".encode()
 enc = f.encrypt(message)
 print(enc)
 print(f.decrypt(enc).decode())
 
-password = b"password" #Ввод пароля
+#password = b"password" #Ввод пароля
+password = "password"
+password = password.encode()
 hashed = bcrypt.hashpw(password, bcrypt.gensalt()) #Хэшированый пароль
 if bcrypt.checkpw(password, hashed): #Проверка пароля
      print("подходит")
 else:
-     print("не подходит")
+     print("не подходит") """
