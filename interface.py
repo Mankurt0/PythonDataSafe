221f736cc59e09f6e4d9a90ffef266294e08b807
Mankurt0 <183689932+mankurt0@users.noreply.github.com>
2025-02-15 10:34:57 +0300

    Реворк главного окна

--- interface.py
+++ interface.py
@@ -5,6 +5,26 @@ root.title('Data safe')
 root.geometry("600x500")
 #root.resizable(False, False)
 root.iconbitmap(default="icon.ico")
+root.rowconfigure(index=1, weight=1)
+root.columnconfigure(index=1, weight=1)
+
+main_menu = Menu()
+create_menu = Menu(tearoff=0)
+delete_menu = Menu(tearoff=0)
+
+create_menu.add_command(label="Новая запись")
+create_menu.add_command(label="Новое изображение")
+
+delete_menu.add_command(label="Удалить запись")
+delete_menu.add_command(label="Удалить изображение")
+
+main_menu.add_command(label="Войти")
+main_menu.add_cascade(label="Создать", menu=create_menu)
+main_menu.add_cascade(label="Удадить", menu=delete_menu)
+main_menu.add_separator()
+main_menu.add_command(label="Выход", command=root.destroy)
+
+root.config(menu=main_menu)
 
 label = Label(root, text = 'Пройдите аутентификацию')
 label.grid(row = 1, column = 1)
